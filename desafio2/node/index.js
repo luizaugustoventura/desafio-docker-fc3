ab54fdebf850466c3a819748c7564cef3c6095b9
Luiz Augusto Ventura <luizaugustoventu1998@hotmail.com>
2022-04-08 00:38:59 -0300

    Additional routes

--- desafio2/node/index.js
+++ desafio2/node/index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql')
 const util = require('util')
 
 const app = express()
+app.use(express.json())
 
 const mysqlConfig = {
     host: 'db',
@@ -12,14 +13,55 @@ const mysqlConfig = {
 }
 
 app.get('/', async (req, res) => {
-    const db = await mysql.createConnection(mysqlConfig)
-    const query = util.promisify(db.query).bind(db)
+    try {
+        const db = await mysql.createConnection(mysqlConfig)
+        const query = util.promisify(db.query).bind(db)
 
-    const result = await query('SELECT (name) FROM people')
+        const result = await query('SELECT (name) FROM people')
 
-    db.end()
+        db.end()
 
-    res.send(`<h1>Hello ${result[0].name}!!!</h1>`)
+        res.send(`<h1>Hello ${result[0].name}!!!</h1>`)
+    } catch (err) {
+        console.error(err)
+        res.send('<h1>Deu ruim :(</h1>')
+    }
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        const name = req.body.name
+
+        const db = await mysql.createConnection(mysqlConfig)
+        const query = util.promisify(db.query).bind(db)
+
+        await query(`INSERT INTO people (name) values ('${name}')`)
+
+        db.end()
+
+        res.send(`<h1>${name} registrado com sucesso!</h1>`)
+    } catch (err) {
+        console.error(err)
+        res.send('<h1>Deu ruim :(</h1>')
+    }
+})
+
+app.get('/search', async (req, res) => {
+    try {
+        const name = req.query.name
+
+        const db = await mysql.createConnection(mysqlConfig)
+        const query = util.promisify(db.query).bind(db)
+
+        const result = await query(`SELECT name FROM people WHERE name="${name}"`)
+
+        db.end()
+
+        res.send(`<h1>Bem-vindo ${result[0].name}!</h1>`)
+    } catch (err) {
+        console.error(err)
+        res.send('<h1>Deu ruim :(</h1>')
+    }
 })
 
 app.listen(3000, async () => {
